0914c79a23abe58b73a724eb12ad5218d4d8b185
mhmohamed <mohamed_hamed4158@yahoo.com>
2019-07-29 14:32:18 +0200

    - Updated to swift 4.2

--- ALCameraViewController/Utilities/PhotoLibraryAuthorizer.swift
+++ ALCameraViewController/Utilities/PhotoLibraryAuthorizer.swift
@@ -1,47 +1,127 @@
 //
-//  PhotoLibraryAuthorizer.swift
-//  ALCameraViewController
+//  ALImagePickerViewController.swift
+//  ALImagePickerViewController
 //
-//  Created by Alex Littlejohn on 2016/03/26.
-//  Copyright © 2016 zero. All rights reserved.
+//  Created by Alex Littlejohn on 2015/06/09.
+//  Copyright (c) 2015 zero. All rights reserved.
 //
 
 import UIKit
 import Photos
 
-public typealias PhotoLibraryAuthorizerCompletion = (NSError?) -> Void
+internal let ImageCellIdentifier = "ImageCell"
 
-class PhotoLibraryAuthorizer {
+internal let defaultItemSpacing: CGFloat = 1
 
-    private let errorDomain = "com.zero.imageFetcher"
+public typealias PhotoLibraryViewSelectionComplete = (PHAsset?) -> Void
 
-    private let completion: PhotoLibraryAuthorizerCompletion
-
-    init(completion: @escaping PhotoLibraryAuthorizerCompletion) {
-        self.completion = completion
-        handleAuthorization(status: PHPhotoLibrary.authorizationStatus())
+public class PhotoLibraryViewController: UIViewController {
+    
+    internal var assets: PHFetchResult<PHAsset>? = nil
+    
+    public var onSelectionComplete: PhotoLibraryViewSelectionComplete?
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.itemSize = CameraGlobals.shared.photoLibraryThumbnailSize
+        layout.minimumInteritemSpacing = defaultItemSpacing
+        layout.minimumLineSpacing = defaultItemSpacing
+        layout.sectionInset = UIEdgeInsets.zero
+        
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        return collectionView
+    }()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setNeedsStatusBarAppearanceUpdate()
+        
+        let buttonImage = UIImage(named: "libraryCancel", in: CameraGlobals.shared.bundle, compatibleWith: nil)?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: buttonImage,
+                                                           style: UIBarButtonItem.Style.plain,
+                                                           target: self,
+                                                           action: #selector(dismissLibrary))
+        
+        view.backgroundColor = UIColor(white: 0.2, alpha: 1)
+        view.addSubview(collectionView)
+        
+        _ = ImageFetcher()
+            .onFailure(onFailure)
+            .onSuccess(onSuccess)
+            .fetch()
     }
     
-    func onDeniedOrRestricted(completion: PhotoLibraryAuthorizerCompletion) {
-        let error = errorWithKey("error.access-denied", domain: errorDomain)
-        completion(error)
+    public override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        collectionView.frame = view.bounds
     }
     
-    func handleAuthorization(status: PHAuthorizationStatus) {
-        switch status {
-        case .notDetermined:
-            PHPhotoLibrary.requestAuthorization(handleAuthorization)
-            break
-        case .authorized:
-            DispatchQueue.main.async {
-                self.completion(nil)
+    public override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    public func present(_ inViewController: UIViewController, animated: Bool) {
+        let navigationController = UINavigationController(rootViewController: self)
+        navigationController.navigationBar.barTintColor = UIColor.black
+        navigationController.navigationBar.barStyle = UIBarStyle.black
+        inViewController.present(navigationController, animated: animated, completion: nil)
+    }
+    
+    @objc public func dismissLibrary() {
+        onSelectionComplete?(nil)
+    }
+    
+    private func onSuccess(_ photos: PHFetchResult<PHAsset>) {
+        assets = photos
+        configureCollectionView()
+    }
+    
+    private func onFailure(_ error: NSError) {
+        let permissionsView = PermissionsView(frame: view.bounds)
+        permissionsView.titleLabel.text = localizedString("permissions.library.title")
+        permissionsView.descriptionLabel.text = localizedString("permissions.library.description")
+        
+        view.addSubview(permissionsView)
+    }
+    
+    private func configureCollectionView() {
+        collectionView.register(ImageCell.self, forCellWithReuseIdentifier: ImageCellIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    internal func itemAtIndexPath(_ indexPath: IndexPath) -> PHAsset? {
+        return assets?[(indexPath as NSIndexPath).row]
+    }
+}
+
+// MARK: - UICollectionViewDataSource -
+extension PhotoLibraryViewController : UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return assets?.count ?? 0
+    }
+    
+    @objc(collectionView:willDisplayCell:forItemAtIndexPath:) public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if cell is ImageCell {
+            if let model = itemAtIndexPath(indexPath) {
+                (cell as! ImageCell).configureWithModel(model)
             }
-            break
-        case .denied, .restricted:
-            DispatchQueue.main.async {
-                self.onDeniedOrRestricted(completion: self.completion)
-            }
-            break
         }
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: ImageCellIdentifier, for: indexPath)
+    }
+}
+
+// MARK: - UICollectionViewDelegate -
+extension PhotoLibraryViewController : UICollectionViewDelegateFlowLayout {
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        onSelectionComplete?(itemAtIndexPath(indexPath))
     }
 }
